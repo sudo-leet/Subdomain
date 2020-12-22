a720dc5dd3e7e29dd4e08d3c0619aa713203e734
Sudo Leet <67073882+sudo-leet@users.noreply.github.com>
2020-12-22 16:02:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-https://github.com/projectdiscovery/subfinder.git; cd subfinder/v2/cmd/subfinder; go build; mv subfinder /usr/local/bin/; subfinder -version
+git clone https://github.com/projectdiscovery/subfinder.git; cd subfinder/v2/cmd/subfinder; go build; mv subfinder /usr/local/bin/; subfinder -version
 
-https://github.com/tomnomnom/assetfinder.git; go build; mv assetfinder /usr/local/bin; assetfinder -version
+git clone https://github.com/tomnomnom/assetfinder.git; go build; mv assetfinder /usr/local/bin; assetfinder -version
